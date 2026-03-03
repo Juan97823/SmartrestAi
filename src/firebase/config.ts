9f2a5584ebcd0a2906f3df75720c4fd6adb491aa
Juan Esteban Alfonso B <juanesab423@gmail.com>
2026-03-03 00:31:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2431477337-2bdce",
+  "appId": "1:342342172088:web:1ae0f92d12e62a783b234d",
+  "apiKey": "AIzaSyDx3mPFk0pf1Uf3RpYgbSCKRy02q8NSjAM",
+  "authDomain": "studio-2431477337-2bdce.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "342342172088"
+};
